9f31ba80e9b3ee50f6170223e18f0103a04f7643
Aarón Tovar <faaronts@gmail.com>
2023-11-29 10:56:53 +0100

    Adds Singleton to influxdb

--- common/influxdb.py
+++ common/influxdb.py
@@ -1,13 +1,21 @@
 from influxdb_client import InfluxDBClient
 
-class InfluxDBSender:
-    def __init__(self, host, token, organization, bucket, metric_prefix):
-        self.host = host
-        self.token = token
-        self.organization = organization
-        self.bucket = bucket
-        self.metric_prefix = metric_prefix
-        self.client = None
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class InfluxDBSender(metaclass=Singleton):
+    def __init__(self, host=None, token=None, organization=None, bucket=None, metric_prefix=None):
+        if host and token and organization and bucket and metric_prefix:
+            self.host = host
+            self.token = token
+            self.organization = organization
+            self.bucket = bucket
+            self.metric_prefix = metric_prefix
+            self.client = None
 
     def initialize_sender(self):
         if self.client:
